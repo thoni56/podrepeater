aac80dffc8d5879445eb13cb9d38519ae00da049
Thomas Nilefalk <thomas@nilefalk.se>
2021-02-24 00:59:34 +0100

    Populate search tab with matches

--- index.js
+++ index.js
@@ -18,3 +18,40 @@ tabs.forEach((tab) => {
         })
     })
 });
+
+// Search
+
+function populate(data) {
+    const matches = data.results;
+    const searchMatches = document.querySelector('#search-matches')
+
+    while (searchMatches.firstChild) {
+        searchMatches.firstChild.remove()
+    }
+
+    const t1 = '<div class="card-image"><figure class="image is-square"><img src="'
+    const t2 = '"></figure></div><div class="card-stacked"><div class="card-content"><div class="media-content"><p class="title is-4">'
+    const t3 = '</p><p class="subtitle is-6">'
+    const t4 = '</p></div><div class="content"><p class="is-italic">'
+    const t5 = '</p></div></div></div>'
+
+    matches.forEach((match) => {
+        let listElement = document.createElement('div')
+        listElement.className = "card is-horizontal"
+        listElement.innerHTML = t1 + match.artworkUrl100 + t2 + match.collectionName + t3 + match.artistName + t4 + match.primaryGenreName + t5;
+        searchMatches.appendChild(listElement);
+    })
+}
+const url = 'https://itunes.apple.com/search?media=podcast&limit=200&term=';
+
+async function fetchPodcasts(term) {
+    const response = await fetch(url + term);
+    return response.json();
+}
+
+const searchButton = document.querySelector('#search-button')
+searchButton.addEventListener('click', (e) => {
+    const term = document.getElementById('search-field').value;
+    fetchPodcasts(term).then(data => populate(data));
+})
+
