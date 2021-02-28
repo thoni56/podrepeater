713ec9308707427b4888dd80258574eeaa1da257
Thomas Nilefalk <thomas@nilefalk.se>
2021-02-28 09:42:14 +0100

    Add a search field

--- src/components/Podcasts.vue
+++ src/components/Podcasts.vue
@@ -1,13 +1,34 @@
 <template>
-  <v-card>Podcasts</v-card>
+  <v-container>
+    <v-card>
+      <v-card-title>Podcasts</v-card-title>
+      <v-text-field
+        v-model="searchString"
+        label="Search for podcasts"
+        single-line
+        hide-details
+        append-icon="search"
+        v-on:keyup.enter="doSearch"
+      >
+      </v-text-field>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
 export default {
   data: function () {
-    return {};
+    return {
+      searchString: "",
+    };
+  },
+  methods: {
+    doSearch: function () {
+      console.log(this.searchString);
+    },
   },
 };
 </script>
 
-<style></style>
+<style>
+</style>
