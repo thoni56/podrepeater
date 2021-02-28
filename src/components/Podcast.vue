ca3a568f194dcb854a14eec1fcba7205ad3d4ba9
Thomas Nilefalk <thomas@nilefalk.se>
2021-02-28 09:55:14 +0100

    Add Podcast component with dummy data

---
+++ src/components/Podcast.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card>
+    <v-card-title>{{ title }}</v-card-title>
+    <v-card-text> {{ description }}</v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Podcast",
+  data: function () {
+    return {
+      title: "Title of Podcast",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ut reprehenderit doloremque odit consequatur amet?",
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/components/Podcasts.vue
+++ src/components/Podcasts.vue
@@ -2,21 +2,32 @@
   <v-container>
     <v-card>
       <v-card-title>Podcasts</v-card-title>
-      <v-text-field
-        v-model="searchString"
-        label="Search for podcasts"
-        single-line
-        hide-details
-        append-icon="search"
-        v-on:keyup.enter="doSearch"
-      >
-      </v-text-field>
+      <v-container>
+        <v-text-field
+          v-model="searchString"
+          label="Search for podcasts"
+          single-line
+          hide-details
+          append-icon="search"
+          v-on:keyup.enter="doSearch"
+        >
+        </v-text-field>
+      </v-container>
     </v-card>
+    <v-container>
+      <Podcast />
+    </v-container>
   </v-container>
 </template>
 
 <script>
+import Podcast from "./Podcast.vue";
+
 export default {
+  name: "Podcasts",
+  components: {
+    Podcast,
+  },
   data: function () {
     return {
       searchString: "",
