9f6cb3479d8dee286968a2605ed30865bc66b856
Thomas Nilefalk <thomas@nilefalk.se>
2021-02-28 23:37:14 +0100

    Add an EpisodeItem class

---
+++ src/classes/EpisodeItem.js
@@ -0,0 +1,8 @@
+module.exports = class EpisodeItem {
+  constructor(item) {
+    this.id = item.id;
+    this.title = item.title;
+    this.description = item.description;
+    this.image = item.image;
+  }
+};
