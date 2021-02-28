6720424ea06e6fe1bacdf000df78152435654bdb
Thomas Nilefalk <thomas@nilefalk.se>
2021-02-28 10:17:37 +0100

    Style Podcast with image

--- src/components/Podcast.vue
+++ src/components/Podcast.vue
@@ -1,18 +1,25 @@
 <template>
   <v-card>
-    <v-card-title>{{ title }}</v-card-title>
-    <v-card-text> {{ description }}</v-card-text>
+    <div class="d-flex no-wrap">
+      <v-avatar tile class="ma-1" size="125">
+        <v-img :src="image"> </v-img>
+      </v-avatar>
+      <div class="text-left">
+        <v-card-title class="pt-0">{{ title }}</v-card-title>
+        <v-card-subtitle> {{ description }}</v-card-subtitle>
+      </div>
+    </div>
   </v-card>
 </template>
 
 <script>
 export default {
-  name: "Podcast",
   data: function () {
     return {
+      image: "https://cdn.vuetifyjs.com/images/cards/store.jpg",
       title: "Title of Podcast",
       description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ut reprehenderit doloremque odit consequatur amet?",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ut reprehenderit doloremque odit consequatur amet? Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ut reprehenderit doloremque odit consequatur amet?",
     };
   },
 };
