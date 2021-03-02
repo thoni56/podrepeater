e1e3c3303bcc12854a9ddb5fe8285d5628ecbca6
Thomas Nilefalk <thomas@nilefalk.se>
2021-03-02 08:04:19 +0100

    [tabs][refator] Tabs into explicit component with computed props

--- src/components/PodRepeater.vue
+++ src/components/PodRepeater.vue
@@ -5,13 +5,18 @@
         <v-tab v-for="item in items" :key="item">{{ item }}</v-tab>
       </v-tabs>
       <v-tabs-items v-model="tab">
-        <v-tab-item v-for="item in items" :key="item">
-          <component
-            v-bind:is="item"
-            @podcast-selected="onPodcastSelected"
+        <v-tab-item key="Podcasts">
+          <Podcasts @podcast-selected="onPodcastSelected" ref="podcasts" />
+        </v-tab-item>
+        <v-tab-item key="Episodes">
+          <Episodes
             @episode-selected="onEpisodeSelected"
-            ref="tab"
+            :podcastItem="podcastItem"
+            ref="episodes"
           />
+        </v-tab-item>
+        <v-tab-item key="Repeats">
+          <Repeats ref="repeats" />
         </v-tab-item>
       </v-tabs-items>
     </v-container>
@@ -22,6 +27,7 @@
 import Podcasts from "./Podcasts.vue";
 import Episodes from "./Episodes.vue";
 import Repeats from "./Repeats.vue";
+import PodcastItem from "../classes/PodcastItem";
 
 export default {
   components: {
@@ -36,7 +42,14 @@ export default {
     return {
       tab: 0,
       items: ["Podcasts", "Episodes", "Repeats"],
+      podcastId: 0,
+      selectedPodcastItem: PodcastItem,
     };
+  },
+  computed: {
+    podcastItem() {
+      return this.selectedPodcastItem;
+    },
   },
   methods: {
     onPodcastSelected: function(podcastId) {
