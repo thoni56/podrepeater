0eaa7a6ea00f1b6536ba37e9efe27c2f56705f4d
Thomas Nilefalk <thomas@nilefalk.se>
2021-03-02 08:18:22 +0100

    [data][episodes] Use feedImage if no episode image

--- src/classes/EpisodeItem.js
+++ src/classes/EpisodeItem.js
@@ -3,6 +3,6 @@ module.exports = class EpisodeItem {
     this.id = item.id;
     this.title = item.title;
     this.description = item.description;
-    this.image = item.image;
+    this.image = item.image != "" ? item.image : item.feedImage;
   }
 };
