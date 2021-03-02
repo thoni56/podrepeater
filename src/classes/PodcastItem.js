107070ef5da37cb0a2780d4cd23b86e928c0de09
Thomas Nilefalk <thomas@nilefalk.se>
2021-03-02 07:57:12 +0100

    [data] Add a few Podcast data fields

--- src/classes/PodcastItem.js
+++ src/classes/PodcastItem.js
@@ -2,8 +2,10 @@ module.exports = class PodcastItem {
   constructor(item) {
     this.id = item.id;
     this.title = item.title;
+    this.author = item.author;
     this.description = item.description;
     this.image = item.image;
     this.categories = item.categories;
+    this.language = item.language;
   }
 };
