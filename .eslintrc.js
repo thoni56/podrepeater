613dcadcd66192d60b641d08332b102725f03b77
Thomas Nilefalk <thomas@nilefalk.se>
2021-03-03 12:33:41 +0100

    [dev][format] Trying to fix conflicts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,16 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/recommended", "eslint:recommended", "prettier/vue", "plugin:prettier/recommended"],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "babel-eslint",
   },
+  plugins: ["prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-unused-vars": "warn",
+  },
 };
