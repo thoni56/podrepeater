eb09db80465cdf7c3201e8c9629256aa4b0016c3
Thomas Nilefalk <thomas@nilefalk.se>
2021-03-07 11:36:02 +0100

    [dev] Remove population of Episodes from Episodes

--- src/components/Episodes.vue
+++ src/components/Episodes.vue
@@ -20,10 +20,6 @@
 import Podcast from "./Podcast.vue";
 import PodcastItem from "../classes/PodcastItem";
 import Episode from "./Episode.vue";
-import { fetchEpisodesFromPodcastIndex } from "../classes/PodcastIndex";
-import EpisodeItem from "../classes/EpisodeItem";
-
-let view = this;
 
 export default {
   components: {
@@ -35,26 +31,11 @@ export default {
     episodes: { type: Array, default: () => [] }
   },
   methods: {
-    populate(podcastItem) {
-      fetchEpisodesFromPodcastIndex(podcastItem.id).then(data => {
-        view = this;
-        view.episodes = [];
-        populateEpisodes(data.items);
-      });
-    },
     onSelected(episodeItem) {
       this.$emit("episode-selected", episodeItem);
     }
   }
 };
-
-function populateEpisodes(data) {
-  const episodes = data.items;
-  episodes.reverse();
-  for (const episode of episodes.values()) {
-    view.episodes.push(new EpisodeItem(episode));
-  }
-}
 </script>
 
 <style></style>
